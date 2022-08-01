c1fcf5022e4640cd7ee00122a99d2f5420f7cc34
ianchb <admin@cgtsoft.com>
2022-08-01 17:04:32 +0800

    Fix error

--- util/validate.php
+++ util/validate.php
@@ -21,13 +21,16 @@ function validate($mode) {
     $config = getConfig();
     if (empty($config['enable'][$mode])) return ;
     $secret = $config['secret'];
-    if(empty($_POST['recaptcha'])){
-    message('recaptcha-box', '请完成人机验证');
+    if (!isset($_POST['recaptcha']) || empty($_POST['recaptcha']))
+    {
+        message('recaptcha-box', '请完成人机验证');
     }
-$verifyResponse = file_get_contents('https://www.recaptcha.net/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['recaptcha']);
-$result = json_decode($verifyResponse);
-    if(!$result->success){
-        message('recaptcha-box', '人机验证无效，请重新验证');
+    else
+    {
+        $verifyResponse = file_get_contents('https://www.recaptcha.net/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['recaptcha']);
+        $result = json_decode($verifyResponse);
+        if(!$result->success){
+            message('recaptcha-box', '人机验证无效，请重新验证');
+        }
     }
 }
-
